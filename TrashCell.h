64bf42260755d83061a1e38cc6e96f01ddd39729
laryufk <pisnq@99experts.com>
2013-09-02 18:26:56 +0900

    Create TrashCell.h

---
+++ TrashCell.h
@@ -0,0 +1,25 @@
+
+#import "TrashCell.h"
+#import "TrashViewController.h"
+
+@implementation TrashCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect
+ {
+ // Drawing code
+ }
+ */
+
+@end
